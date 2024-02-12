cf3fb7884ae173657a023b30923709e94ec84e5f
Alberto Paros <mib.albert@gmail.com>
2024-02-12 13:27:45 +0200

    pushed

--- app/page.jsx
+++ app/page.jsx
@@ -3,24 +3,22 @@
  */
 
 import ContactUsInsidePage from "@/components/contact-us/contact-us-inside-page";
-import {
-  Hero,
-  Elevator,
-  PeaceOfMind,
-  Numbers,
-  Plans,
-  Reviews,
-  LandingFooter,
-} from "@/components/landing-page/index";
+import Elevator from "@/components/landing-page/elevator";
+import Hero from "@/components/landing-page/hero";
+import LandingFooter from "@/components/landing-page/landing-footer";
+import Numbers from "@/components/landing-page/numbers";
+import PeaceOfMind from "@/components/landing-page/peace-of-mind";
+import Plans from "@/components/landing-page/plans";
+import Reviews from "@/components/landing-page/reviews";
 
 export default async function Home() {
-  let data;
-  try {
-    const res = await prisma.home.findMany();
-    if (res) data = res;
-  } catch (error) {
-    console.log(error);
-  }
+  // let data;
+  // try {
+  //   const res = await prisma.home.findMany();
+  //   if (res) data = res;
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   return (
     <section className=" ">
