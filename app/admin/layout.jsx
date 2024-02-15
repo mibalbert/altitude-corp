b6490fd3afd337859fce45a9f093e5ca395ba2f4
Alberto Paros <mib.albert@gmail.com>
2024-02-15 03:31:03 +0200

    testing deployment

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -4,7 +4,6 @@
 
 import React from "react";
 import { SearchCommand } from "@/components/admin/modals-and-commands/search-command";
-import AdminSideNav from "@/components/admin/admin-sidenav/admin-sidenav";
 import { Navigation } from "@/components/admin/admin-sidenav/navigation";
 
 const layout = async ({ children }) => {
@@ -12,7 +11,6 @@ const layout = async ({ children }) => {
     <>
       <div className="h-full max-w-[1600px]  w-full mx-auto flex dark:bg-[#1F1F1F]">
         <Navigation />
-        {/* <AdminSideNav /> */}
         <main className="flex-1 h-full overflow-y-auto">
           <SearchCommand />
           {children}
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,12 +2,8 @@
  * admin/page.jsx
  */
 
-import prisma from "@/lib/prismadb";
-import CreatePost from "@/components/admin/home-create-post";
 
 export default async function AdminHome() {
-  const posts = await prisma.post.findMany();
-
   return (
     <div>
       <h1>Dashboard</h1>
