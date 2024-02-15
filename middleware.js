738fb9c7b8ebc03a9906450a6167d07d188275e2
Alberto Paros <mib.albert@gmail.com>
2024-02-15 03:35:58 +0200

    testing deployment

--- middleware.js
+++ middleware.js
@@ -7,46 +7,46 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
 
-  // function middleware(req) {
-  //   // if (
-  //   //   req.nextUrl.pathname.startsWith("/user") &&
-  //   //   req.nextauth.token?.role !== "USER"
-  //   // ) {
-  //   //   return NextResponse.redirect(
-  //   //     //Use this type of url if you want to extract the error message and display it
-  //   //     // new URL("/auth/signIn?error=You_Are_Not_Authorized!&message2=Sign_In_with_the_proper_credentials_to_gain_access", req.url)
-  //   //     new URL("/", req.url),
-  //   //   );
-  //   // }
-  //   // if (
-  //   //   req.nextUrl.pathname.startsWith("/writer") &&
-  //   //   req.nextauth.token?.role !== "WRITER"
-  //   // ) {
-  //   //   return NextResponse.redirect(
-  //   //     //Use this type of url if you want to extract the error message and display it
-  //   //     // new URL("/auth/signIn?error=You_Are_Not_Authorized!&message2=Sign_In_with_the_proper_credentials_to_gain_access", req.url)
-  //   //     new URL("/", req.url),
-  //   //   );
-  //   // }
-  //   if (
-  //     req.nextUrl.pathname.startsWith("/admin") &&
-  //     req.nextauth.token?.role !== "ADMIN"
-  //   ) {
-  //     return NextResponse.redirect(
-  //       //Use this type of url if you want to extract the error message and display it
-  //       // new URL("/auth/signIn?error=You_Are_Not_Authorized!&message2=Sign_In_with_the_proper_credentials_to_gain_access", req.url)
-  //       new URL("/", req.url),
-  //     );
-  //   }
-  //   return null;
-  // },
-  // {
-  //   callbacks: {
-  //     authorized: ({ token }) => {
-  //       return !!token;
-  //     },
-  //   },
-  // }
+  function middleware(req) {
+    // if (
+    //   req.nextUrl.pathname.startsWith("/user") &&
+    //   req.nextauth.token?.role !== "USER"
+    // ) {
+    //   return NextResponse.redirect(
+    //     //Use this type of url if you want to extract the error message and display it
+    //     // new URL("/auth/signIn?error=You_Are_Not_Authorized!&message2=Sign_In_with_the_proper_credentials_to_gain_access", req.url)
+    //     new URL("/", req.url),
+    //   );
+    // }
+    // if (
+    //   req.nextUrl.pathname.startsWith("/writer") &&
+    //   req.nextauth.token?.role !== "WRITER"
+    // ) {
+    //   return NextResponse.redirect(
+    //     //Use this type of url if you want to extract the error message and display it
+    //     // new URL("/auth/signIn?error=You_Are_Not_Authorized!&message2=Sign_In_with_the_proper_credentials_to_gain_access", req.url)
+    //     new URL("/", req.url),
+    //   );
+    // }
+    if (
+      req.nextUrl.pathname.startsWith("/admin") &&
+      req.nextauth.token?.role !== "ADMIN"
+    ) {
+      return NextResponse.redirect(
+        //Use this type of url if you want to extract the error message and display it
+        // new URL("/auth/signIn?error=You_Are_Not_Authorized!&message2=Sign_In_with_the_proper_credentials_to_gain_access", req.url)
+        new URL("/", req.url),
+      );
+    }
+    return null;
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        return !!token;
+      },
+    },
+  }
 );
 
 
